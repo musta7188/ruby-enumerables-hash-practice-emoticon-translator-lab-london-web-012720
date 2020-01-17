1a2f0f6f389cb74d782c7a9aa26234f73dc19cb7
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-17 18:54:19 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,65 @@
-# require modules here
+require "yaml"
+require "pry"
 
-def load_library
-  # code goes here
+def load_library(emotion)
+file  = YAML.load_file(emotion)
+
+main_hash = {
+get_meaning: {},
+get_emoticon: {}
+}
+
+file.each do |meaning, emotion|
+
+
+  main_hash[:get_meaning][emotion[1]] = meaning
+  main_hash[:get_emoticon][emotion[0]] = emotion[1]
+
+end
+main_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+file_call = YAML.load_file(File.open(File.join(File.dirname(__FILE__),'emoticons.yml')))
+
+
+
+def get_japanese_emoticon(file, emotion)
+
+
+
+library =  load_library(file)
+
+library.each  do |keys, mean_emo|
+
+  mean_emo.map do |list|
+
+   list.each do |emo|
+ found = list.any?(emotion)
+
+ if found === true
+   return library[keys][emo]
+        end
+      end
+    end
+  end
+  return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file, emoticon)
+library = load_library(file)
+
+library.each do |keys, hashes|
+  hashes.map do |list|
+list.each do |eng|
+
+found = list.any?(emoticon)
+
+if found === true
+  return library[keys][eng]
+end
+
+end
+end
+end
+return "Sorry, that emoticon was not found"
 end
